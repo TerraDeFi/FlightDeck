55ed02dc0bc188a39361d0e6ff50b4474b04ab82
montecarlocode <monte00829@gmail.com>
2022-05-01 09:02:47 +0800

    Statusupdown

--- src/components/Statusupdown.js
+++ src/components/Statusupdown.js
@@ -3,22 +3,22 @@ import { BsArrowDown, BsArrowUp } from "react-icons/bs";
 
 const Statusupdown = ({ id, primary, current }) => {
 
-    const [upDown, setUpDown] = useState(0);
-    const [percent, setPercent] = useState(0);
-    let float_percent = Number.parseFloat(current) / Number.parseFloat(primary) * 100 -100;
+    const [upDown, setUpDown] = useState(1);
+    const [percent, setPercent] = useState(Number.parseInt(Number.parseFloat(current) / Number.parseFloat(primary) * 100 -100));
+    // let float_percent = Number.parseFloat(current) / Number.parseFloat(primary) * 100 -100;
+    
     useEffect(() => {
-        setPercent(Number.parseInt(float_percent));
-        if (percent < 0){
-            setUpDown(-1);
-        } else if(percent > 0){
+        // setPercent(Number.parseInt(Number.parseFloat(current) / Number.parseFloat(primary) * 100 -100));
+        if(percent > 0){
+
             setUpDown(1);
         } else {
+
             setUpDown(0);
         }
     }, [])
-    
-    return ( 
-        upDown >  0 ? (<span style={{marginLeft: '3px', color: "#2FDE00"}}><BsArrowUp />{percent}%</span>) : (<span style={{marginLeft: '3px', color: "red"}}></span>) 
+    return (
+        upDown >  0 ? (<span style={{marginLeft: '3px', color: "#2FDE00"}}><BsArrowUp />{percent}%</span>) : (<span style={{marginLeft: '3px', color: 'dark'}}></span>)
     )
 }
 
